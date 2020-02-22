89b4ac08edd61108e83a76f35b1585d9b07951c1
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-02-22 18:49:49 -0500

    completed nested brackets

--- nested.py
+++ nested.py
@@ -3,21 +3,49 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "mprodhan/yabamov"
 
 import sys
 
+nested_dict = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '>': '<',
+    '*)': '(*'
+}
 
 def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+    count = 0
+    nested_list = []
+    while line:
+        token = line[0]
+        if line.startswith("(*"):
+            token = "(*"
+        if line.startswith("*)"):
+            token = "*)"
+        count += 1
+        if token in nested_dict.values():
+            nested_list.append(token)
+        elif token in nested_dict.keys():
+            a = nested_dict[token]
+            b = nested_list.pop()
+            if a != b:
+                return "NO " + str(count)
+        line = line[len(token):]
+    if nested_list:
+        return "NO " + str(count)
+    else:
+        return "YES"
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
-    # Results: print to console and also write to output file
-    pass
-
+    with open('input.txt', 'r') as f:
+        with open('output.txt', 'w') as w:
+            for line in f:
+                result = is_nested(line)
+                w.write(result+'\n')
 
 if __name__ == '__main__':
     main(sys.argv[1:])
